c3c23c39eb0a8346b8621660af8bec288cdacf04
868jjm <juergenjeanmarie@gmail.com>
2022-11-24 18:18:02 -0400

    24112022617PM

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 
-        
+          
         var scene = new THREE.Scene(); 
 
         var camera = new THREE.PerspectiveCamera(25,window.innerWidth/window.innerHeight,0.1,12000)
@@ -24,9 +24,9 @@
         var mesh = new THREE.Mesh(geometry, material);
         mesh.userData = { URL: "http://stackoverflow.com"};    
         scene.add(mesh);
+        
 
 
-       
 
 
         var light = new THREE.PointLight(0xFFFFFF, 1, 1000)
@@ -194,5 +194,42 @@
         window.addEventListener('load', loadPage);
         
         
+        function loader (css, js) {
+            // (A) TOTAL NUMBER OF SCRIPTS
+            var total = css.length + js.length,
+                now = 0, s;
+          
+            // (B) READY?
+            var ready = () => {
+              now++;
+              // if (now==total) { document.getElementById("loading").remove(); }
+          
+              // FOR THIS DEMO ONLY.
+              // PAGE LOAD IS SO FAST YOU WON'T EVEN SEE THE SPINNER.
+              // SO WE DELAY IT BY 2 SECONDS.
+              if (now==total) {
+                window.setTimeout(()=>{
+                  document.getElementById("loading").remove();
+                }, 100);
+              }
+            };
+          
+            // (C) INSERT <LINK> INTO <HEAD>
+            css.forEach((url, i) => {
+              s = document.createElement("link");
+              s.rel = "stylesheet";
+              s.href = url;
+              s.onload = ready; s.onerror = ready;
+              document.head.appendChild(s);
+            });
+          
+            // (D) INSERT <SCRIPT> INTO <HEAD>
+            js.forEach((url, i) => {
+              s = document.createElement("script");
+              s.src = url;
+              s.onload = ready; s.onerror = ready;
+              document.head.appendChild(s);
+            });
+          }
         render();
                
